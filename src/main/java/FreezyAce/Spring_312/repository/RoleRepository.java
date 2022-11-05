4f03bffa220041ab218a950ca025b9686eb39ae3
FreezyAce <saskia@list.ru>
2022-11-05 16:18:07 +0400

    Done.

--- src/main/java/FreezyAce/Spring_312/repository/RoleRepository.java
+++ src/main/java/FreezyAce/Spring_312/repository/RoleRepository.java
@@ -2,7 +2,8 @@ package FreezyAce.Spring_312.repository;
 
 import FreezyAce.Spring_312.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
